65863a49b29540474920a13cdda821906bc21626
Mykola Dymytrenko <dymytrenko73@icloud.com>
2025-03-13 17:13:25 +0100

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import Tabs from './components/Tabs/Tabs';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Tabs } from './components/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -12,7 +12,6 @@ export const tabs = [
 
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
-  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   const handleTabClick = tabId => {
     if (tabId !== activeTabId) {
@@ -22,17 +21,15 @@ export const App = () => {
 
   return (
     <div className="section">
-      <h1 className="title">Selected tab is {activeTab.title}</h1>
-
+      <h1 className="title">
+        Selected tab is {tabs.find(tab => tab.id === activeTabId)?.title}
+      </h1>
       <div data-cy="TabsComponent">
         <Tabs
           tabs={tabs}
           activeTabId={activeTabId}
-          onTabSelect={handleTabClick}
+          onTabSelected={handleTabClick}
         />
-        <div className="block" data-cy="TabContent">
-          {activeTab.content}
-        </div>
       </div>
     </div>
   );
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,26 +1,37 @@
-export const Tabs = ({ tabs, activeTabId, onTabSelect }) => {
+import React from 'react';
+
+const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
   return (
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            key={tab.id}
-            className={tab.id === activeTabId ? 'is-active' : ''}
-            data-cy="Tab"
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={e => {
-                e.preventDefault();
-                onTabSelect(tab.id);
-              }}
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === activeTabId ? 'is-active' : ''}
+              data-cy="Tab"
             >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={e => {
+                  e.preventDefault();
+                  onTabSelected(tab.id);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
     </div>
   );
 };
+
+export default Tabs;
